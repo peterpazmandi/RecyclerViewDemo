80d1d1054e96f60456b207e9443fc323715239cd
inspire-coding <+PaZ+PeTi+88113+>
2020-03-21 12:53:07 +0100

    Create a data class

---
+++ app/src/main/java/com/inspirecoding/recyclerviewdemo/enums/Prioirities.kt
@@ -0,0 +1,8 @@
+package com.inspirecoding.recyclerview_practice_1.enums
+
+enum class Prioirities
+{
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ app/src/main/java/com/inspirecoding/recyclerviewdemo/model/ToDo.kt
@@ -0,0 +1,14 @@
+package com.inspirecoding.recyclerview_practice_1.model
+
+import android.os.Parcelable
+import com.inspirecoding.recyclerview_practice_1.enums.Prioirities
+import kotlinx.android.parcel.Parcelize
+import java.time.LocalDate
+
+@Parcelize
+data class ToDo(
+    var title: String,
+    var description: String,
+    var dueDate: String,
+    var priority: Prioirities
+) : Parcelable
