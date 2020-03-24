e4d099291cd56f8bcf5adbb95bc7944a0732b4d6
inspire-coding <+PaZ+PeTi+88113+>
2020-03-24 06:10:59 +0100

    Add the FloatingActionButtons

--- app/src/main/java/com/inspirecoding/recyclerviewdemo/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/inspirecoding/recyclerviewdemo/fragments/RecyclerFragment.kt
@@ -13,6 +13,8 @@ import kotlinx.android.synthetic.main.fragment_recycler.*
 
 class RecyclerFragment : Fragment()
 {
+    private var isFabOpen = false
+
     override fun onStart()
     {
         super.onStart()
@@ -30,8 +32,35 @@ class RecyclerFragment : Fragment()
     {
         super.onViewCreated(view, savedInstanceState)
 
+        fab.setOnClickListener {
+            if(isFabOpen)
+            {
+                hideFabMenu()
+            }
+            else
+            {
+                showFabMenu()
+            }
+        }
+
         fab_add.setOnClickListener {
             findNavController().navigate(R.id.action_recyclerFragment_to_addToDoDialog)
+            hideFabMenu()
         }
+    }
+
+    private fun showFabMenu()
+    {
+        isFabOpen = true
+        fab.animate().alpha(0.5f)
+        fab_add.animate().translationY(-resources.getDimension(R.dimen.fab_move_upwards_65))
+        fab_search.animate().translationY(-resources.getDimension(R.dimen.fab_move_upwards_125))
+    }
+    private fun hideFabMenu()
+    {
+        isFabOpen = false
+        fab.animate().alpha(1f)
+        fab_add.animate().translationY(0f)
+        fab_search.animate().translationY(0f)
     }
 }
